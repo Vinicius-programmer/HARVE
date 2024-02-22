c897ae42c1ee8b039fa384b4bff356c0297b4b17
Vinicius-programmer <viniciusmoreira.svy@gmail.com>
2024-02-21 21:21:00 -0300

    Projeto Git.

---
+++ Projeto/script.js
@@ -0,0 +1,9 @@
+function abrirNav() {
+    document.getElementById("menuOculto").style.width="300px";
+    document.getElementById("principal").style.marginRight="300px";
+}
+
+function fecharNav() {
+    document.getElementById("menuOculto").style.width="0";
+    document.getElementById("principal").style.marginRight="0";
+}
